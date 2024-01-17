88b2adbbbb0e29c000dd8e47e62df519cb969491
Ashis Raj <ashis.raj@nuorder.com>
2024-01-17 15:50:42 +0530

    Added DbUtils class for spring-jdbc template example

--- src/test/java/conduitApp/features/DBHandler.feature
+++ src/test/java/conduitApp/features/DBHandler.feature
@@ -4,6 +4,10 @@ Feature: DB
 Background: Before Hook
     * def mysqlDbHandler = Java.type('helpers.MySQLDbHandler')
     * def mongoDbHandler = Java.type('helpers.MongoDBHandler')
+    * def config = { username: 'root', password: 'root123', url: "jdbc:mysql://localhost:3306/test", driverClassName: "com.mysql.cj.jdbc.Driver" }
+    * def DbUtils = Java.type('helpers.DbUtils')
+    * def db = new DbUtils(config)
+
     * def ObjectMapper = Java.type('com.fasterxml.jackson.databind.ObjectMapper')
 
 Scenario: MySQL: Insert Table
@@ -29,3 +33,15 @@ Scenario: MongoDb: Get Document
     * def connectionString = mapper.writeValueAsString(envConfig.mongoDBConfig);
     * def job = mongoDbHandler.getDocuments(connectionString, "{'job': 'Test Automation Architect'}")
     * match job == "#array"
+
+Scenario: DbUtils: Read Rows
+# since the DbUtils returns a Java List (of Map-s), it becomes normal JSON here!
+# which means that you can use the full power of Karate's 'match' syntax
+* def jobs = db.readRows('SELECT * FROM JOBS')
+* match each jobs == { id: '#number', job_desc: '#string', min_level: "#string", max_level: "#string", "create_time": "#null" }
+* id=8
+* def job = db.readRow('SELECT * FROM JOBS J WHERE J.ID=' + id)
+* match job.job_desc == 'UI Designer'
+
+* def test = db.readValue('SELECT ID FROM JOBS J WHERE J.ID=' + id)
+* match test == 8
---
+++ src/test/java/helpers/DbUtils.java
@@ -0,0 +1,46 @@
+package helpers;
+
+import java.util.List;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class DbUtils {
+    
+    private static final Logger logger = LoggerFactory.getLogger(DbUtils.class); 
+    
+    private final JdbcTemplate jdbcTemplate;
+
+    public DbUtils(Map<String, Object> config) {
+        System.out.println(config.get("url"));
+        System.out.println(config.get("username"));
+        System.out.println(config.get("password"));
+        System.out.println(config.get("driverClassName"));
+
+        String url = (String) config.get("url");
+        String username = (String) config.get("username");
+        String password = (String) config.get("password");
+        String driver = (String) config.get("driverClassName");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        logger.info("init jdbc template: {}", url);
+    }
+    
+    public Object readValue(String query) {
+        return jdbcTemplate.queryForObject(query, Object.class);
+    }    
+    
+    public Map<String, Object> readRow(String query) {
+        return jdbcTemplate.queryForMap(query);
+    }
+    
+    public List<Map<String, Object>> readRows(String query) {
+        return jdbcTemplate.queryForList(query);
+    }         
+}
