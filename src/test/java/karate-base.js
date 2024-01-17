8a33dbfa4dd9a5d3abc0a1490fbb91b1ebc89c72
Ashis Raj <ashis.raj@nuorder.com>
2024-01-17 16:49:25 +0530

    Added env folder under config and moved env specific files.

--- src/test/java/karate-base.js
+++ src/test/java/karate-base.js
@@ -7,7 +7,7 @@ function fn() {
     var env = karate.env || 'qa'; // get system property 'karate.env', else default to 'qa'
     karate.log('karate.env system property was: ', env);
 
-    var envConfig = read(`classpath:config/karate-config-${env}.json`);
+    var envConfig = read(`classpath:config/env/karate-config-${env}.json`);
     var envData = read(`classpath:data/env/${env}.data.json`);
     
     var config = {
